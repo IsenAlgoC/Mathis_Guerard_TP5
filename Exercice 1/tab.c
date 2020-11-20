cab85c7cf41df5bd34626d42a57e25dd0a9fe56e
Mathis GUERARD <mathis.guerard@isen.yncrea.fr>
2020-11-20 08:25:35 +0100

    k

---
+++ Exercice 1/tab.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//fonction qui remplit un tableau d'entiers de taille size avec des zéros
+int initTab(int *Tab, int size) {
+	if (Tab == NULL || size < 0) {
+		return -1;
+	}
+	else {
+		*Tab[0] = 0;
+		return size;
+	}
+}
